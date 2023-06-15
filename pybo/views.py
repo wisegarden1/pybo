5458748aec428fdf4d36e3a695f38de88c971a94
wisegarden1 <wisegarden1@gmail.com>
2023-06-15 14:46:50 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question
+from django.utils import timezone
+
+# Create your views here.
+
+def index(request):
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
+    # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+def answer_create(request, question_id):
+    """
+    pybo 답변 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question.id)
